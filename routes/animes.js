de0ee4a0323301120bb1a35f1b52f886db167708
TeddyTjoe1990 <her21059@byui.edu>
2023-06-28 18:40:43 +0700

    update anime route with authentication

--- routes/animes.js
+++ routes/animes.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const animeController = require('../controllers/animes');
 const validation = require('../midware/validate');
+const {requiresAuth} = require('express-openid-connect');
 
 router.get('/', animeController.getAllAnimes);
 
 router.get('/:id', animeController.getAnimeById);
 
-router.post('/', validation.saveAnime, animeController.createAnime);
+router.post('/', requiresAuth(), validation.saveAnime, animeController.createAnime);
 
-router.put('/:id', validation.saveAnime, animeController.updateAnime);
+router.put('/:id', requiresAuth(), validation.saveAnime, animeController.updateAnime);
 
-router.delete('/:id', animeController.deleteAnime);
+router.delete('/:id', requiresAuth(), animeController.deleteAnime);
 
 module.exports = router;
