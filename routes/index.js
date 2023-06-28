6748969dd2d43e98228cb04c89a1b0afd05dbd47
TeddyTjoe1990 <her21059@byui.edu>
2023-06-28 18:17:02 +0700

    added auth on index route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const {auth} = require('express-openid-connect');
+
+router.use(auth(authenticator.config));
+router.use('/', require('./auth'));
 
 router.use('/', require('./swagger'));
 router.use('/anime', require('./animes'));
