a021ee7c3d949997da7c90a525ddd2539945b7f7
TeddyTjoe1990 <her21059@byui.edu>
2023-06-28 18:25:35 +0700

    fixing some bugs

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authenticateController = require('../controllers/auth');
 
-router.get('/', authenticateContoller.loginLogOut);
+router.get('/', authenticateController.loginLogOut);
 
 module.exports = router;
