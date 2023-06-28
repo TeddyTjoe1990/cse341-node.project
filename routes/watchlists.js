97eafafb22528af368869e0438c4a364f6dad384
TeddyTjoe1990 <her21059@byui.edu>
2023-06-28 18:43:39 +0700

    update watchlist route with authentication

--- routes/watchlists.js
+++ routes/watchlists.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const watchlistsController = require('../controllers/watchlists');
 const validation = require('../midware/validate');
+const {requiresAuth} = require('express-openid-connect');
 
 router.get('/', watchlistsController.getAllWatchlists);
 
 router.get('/:id', watchlistsController.getWatchlistById);
 
-router.post('/', validation.saveWatchlist, watchlistsController.createWatchlist);
+router.post('/', requiresAuth(), validation.saveWatchlist, watchlistsController.createWatchlist);
 
-router.put('/:id', validation.saveWatchlist, watchlistsController.updateWatchlist);
+router.put('/:id', requiresAuth(), validation.saveWatchlist, watchlistsController.updateWatchlist);
 
-router.delete('/:id', watchlistsController.deleteWatchlist);
+router.delete('/:id', requiresAuth(), watchlistsController.deleteWatchlist);
 
 module.exports = router;
