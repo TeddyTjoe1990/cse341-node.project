8e6f5103d3350f8e2083afe36eede46f9dd65d0e
TeddyTjoe1990 <her21059@byui.edu>
2023-06-29 09:16:19 +0700

    fixing some words

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateController = require('../controllers/authenticate');
+const authenticateController = require('../controllers/auth');
 
 router.get('/', authenticateController.loginLogout);
 
