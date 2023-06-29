360a150ec1623d584f9b222f3b85d4496a09a720
TeddyTjoe1990 <her21059@byui.edu>
2023-06-29 11:51:00 +0700

    edit auth files in route

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authenticateController = require('../controllers/authenticate');
 
-// req.isAuthenticated is provided from the auth router
 router.get('/', authenticateController.loginLogout);
 
 module.exports = router;
