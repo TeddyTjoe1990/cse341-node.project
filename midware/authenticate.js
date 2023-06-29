e496b32f98a67e172992ea1db24bc7fc7bc21403
TeddyTjoe1990 <her21059@byui.edu>
2023-06-29 12:25:17 +0700

    change somehting

--- midware/authenticate.js
+++ midware/authenticate.js
@@ -5,9 +5,9 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:10000',
+  baseURL: 'https://cse341-project-b2zk.onrender.com',
   clientID: 'c0EB0TJqRNHuoEzqO6eKqEHIWc3UOZuH',
   issuerBaseURL: 'https://dev-js03z6vvwguaxggx.us.auth0.com'
 };
 
-module.exports = { config };
+module.exports = {config};
