487a30b29755fa711110bc06cf74d8c3d44d152f
TeddyTjoe1990 <her21059@byui.edu>
2023-06-29 12:33:00 +0700

    supposed to work properly

--- midware/authenticate.js
+++ midware/authenticate.js
@@ -1,13 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'https://cse341-project-b2zk.onrender.com',
-  clientID: 'c0EB0TJqRNHuoEzqO6eKqEHIWc3UOZuH',
-  issuerBaseURL: 'https://dev-js03z6vvwguaxggx.us.auth0.com'
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
 };
 
 module.exports = {config};
