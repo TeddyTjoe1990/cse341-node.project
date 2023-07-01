70329a894448f36dab4a6d66e1c6b6b6b8651108
TeddyTjoe1990 <her21059@byui.edu>
2023-07-01 12:50:42 +0700

    undo test

--- midware/authenticate.js
+++ midware/authenticate.js
@@ -4,10 +4,10 @@ dotenv.config();
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_RENDER_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'https://cse341-project-b2zk.onrender.com',
+  clientID: 'c0EB0TJqRNHuoEzqO6eKqEHIWc3UOZuH',
+  issuerBaseURL: 'https://dev-js03z6vvwguaxggx.us.auth0.com'
 };
 
 const isAuthenticated = (req, res, next) => {
@@ -17,4 +17,4 @@ const isAuthenticated = (req, res, next) => {
   next();
 };
 
-module.exports = {config, isAuthenticated};
+module.exports = { config, isAuthenticated };
